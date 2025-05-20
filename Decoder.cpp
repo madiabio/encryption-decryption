6c0a670c769e1610fba3158a89bdfd5f82c1935b
madiabio <madelineabio@gmail.com>
2025-05-20 11:57:42 +1000

    began writing decode function

--- Decoder.cpp
+++ Decoder.cpp
@@ -22,7 +22,6 @@ void Decoder::makeGrid()
 {
 	int n = gridSize;
 	int idx = 0;
-	// Access the grid member variable directly or via setGrid/getGrid as needed
 	for (int diag = 0; diag <= 2 * (n - 1); ++diag) {
 		for (int row = 0; row < n; ++row) {
 			int col = diag - row;
@@ -33,3 +32,93 @@ void Decoder::makeGrid()
 	}
 }
 
+void Decoder::decode()
+{
+	// if no fullstop, then the unencrypted message ends in the middle of the grid.
+    bool fullstop = false;
+    int stop_row = gridSize / 2;
+	int stop_col = gridSize / 2;
+
+	// if in last round of decryption, look for fullstop.
+	if (completedRounds == totalRounds-1)
+	{
+		// inspect for fullstop.
+		for (auto& ch : encryptedMsg) 
+		{
+			if (ch == '.') 
+			{ 
+				fullstop = true; 
+				break;
+			}
+		}
+
+		// find coordinate of fullstop. (can probs remove this actually)
+		if (fullstop)
+		{
+			bool found = false;
+			for (int i = 0; i < gridSize; ++i)
+			{
+				for (int j = 0; j < gridSize; ++j)
+				{
+					if (grid[i][j] == '.')
+					{
+						stop_row = i;
+						stop_col = j;
+						found = true;
+						break;
+					}
+				}
+				if (found) { break;} // break once location of fullstop is found.
+			}
+		}
+	}
+
+	// FROM ENCODER:
+    int i = 0;
+    int starting_col = 0;
+    bool fullstop_located = false;
+    std::string& decryptedMsg;
+	int row;
+
+    while ((i < msg.length() || !fullstop_located) && starting_col < (gridSize / 2)) {
+        int top_row_of_layer = starting_col;
+        int current_col = starting_col;
+
+        // up & right diagonal
+        for (row = gridSize / 2; row > top_row_of_layer; --row) {
+            decryptedMsg += grid[row][current_col];
+            if (grid[row][current_col] == '.' && fullstop) { break; }
+            current_col++;
+        }
+
+        // down & right diagonal
+        for (row = top_row_of_layer; row < gridSize / 2; ++row) {
+            decryptedMsg += grid[row][current_col];
+            if (grid[row][current_col] == '.' && fullstop) { break; }
+            current_col++;
+        }
+
+        int bottom_row_of_layer = gridSize - top_row_of_layer;
+
+        // down & left diagonal
+        for (row = gridSize / 2; row < bottom_row_of_layer - 1; ++row) {
+			decryptedMsg += grid[row][current_col];
+			if (grid[row][current_col] == '.' && fullstop) { break; }
+			current_col--;
+        }
+
+        // up & left diagonal
+        for (row = bottom_row_of_layer - 1; row > gridSize / 2; --row) {
+			decryptedMsg += grid[row][current_col];
+			if (grid[row][current_col] == '.' && fullstop) { break; }
+            current_col--;
+        }
+
+        starting_col++;
+        if (starting_col == gridSize / 2) {
+			decryptedMsg += grid[row][current_col];
+			if (grid[row][current_col] == '.' && fullstop) { break; }
+        }
+    }
+
+}
---
+++ Encoder.h
@@ -0,0 +1,72 @@
+/// Header file for the encoder class.
+#pragma once
+#include "MessageHandler.h"
+#include <vector>
+#include <string>
+
+/// <summary>
+/// Class with tools to encrypt a message using the encryption algorithm.
+/// </summary>
+class Encoder : public MessageHandler
+{
+private:
+    /// <summary>
+    /// Helper function to remove whitespace before encryption.
+    /// </summary>
+    /// <param name="str">String to remove whitespace from.</param>
+    /// <returns></returns>
+    static std::string removeWhitespace(std::string str);
+
+    /// <summary>
+    /// Gets a random upper case letter between A-Z.
+    /// </summary>
+    /// <returns>Returns a random upper case letter.</returns>
+    virtual char getRandomLetter();
+    /// <summary>
+
+    /// Creates or initializes a grid structure. Overrides a virtual function from a base class.
+    /// This method differs from the base class by first initializing the grid using random letters,
+    /// then filling it with the encrypted message.
+    /// It can only be called after the encrypted message is complete.
+    /// </summary>
+    void makeGrid() override;
+
+    /// <summary>
+    /// Collapses the grid into an encrypted message string.
+    /// </summary>
+    void encode();
+
+public:
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    Encoder() : MessageHandler() {}
+
+    /// <summary>
+    /// Constructor that takes in an unencrypted message, the number of desired encryption rounds, and the grid size.
+    /// To be used with automatic grid sizing.
+    /// Handles if a message has whitespace or needs to have a fullstop appended.
+    /// </summary>
+    /// <param name="m">Unencryped message</param>
+    /// <param name="r">Number of encryption rounds</param> 
+    Encoder(const std::string& m, int r) : MessageHandler(removeWhitespace(m), "", r) { makeGrid(); }
+
+    /// <summary>
+    /// Constructor that takes in an unencrypted message, the number of desired encryption rounds, and the grid size.
+    /// To be used with manual grid sizing.
+    /// Handles if a message has whitespace or needs to have a fullstop appended.
+    /// </summary>
+    /// <param name="m">Unencryped message</param>
+    /// <param name="r">Number of encryption rounds</param> 
+    /// <param name="g">Size of grid</param>
+    Encoder(const std::string& m, int r, int g) : MessageHandler(removeWhitespace(m), "", r, g) { makeGrid(); }
+    
+    /// <summary>
+    /// Performs encrpytion based upon the number of encryption rounds. Updates the encryptedMsg member.
+    /// </summary>
+    void encrypt();
+
+};
+
+
